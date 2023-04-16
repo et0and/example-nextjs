00a6786bc2ea84881f48e44de01751315fdf8fca
Tom Hackshaw <tom@tomhackshaw.com>
2023-04-16 17:21:20 +1200

    Bit more

--- api/upload.js
+++ api/upload.js
@@ -1,76 +1,30 @@
-// api/upload.js
-import { createError } from 'micro';
-import { uploader } from 'micro-upload';
-import fetch from 'node-fetch';
-import FormData from 'form-data';
+// /api/upload.js
+const B2 = require('b2-sdk');
 
-const upload = uploader({});
+const b2 = new B2({
+  applicationKeyId: process.env.B2_APPLICATION_KEY_ID,
+  applicationKey: process.env.B2_APPLICATION_KEY
+});
 
-export default async function handler(req, res) {
-  try {
-    const files = await upload(req, res);
+module.exports = async (req, res) => {
+  if (req.method === 'POST') {
+    const fileName = req.body.filename;
+    const fileContent = req.body.file;
 
-    if (!files) {
-      throw createError(400, 'No file received');
-    }
+    // Authorize and get the URL to upload the file
+    await b2.authorize();
+    const uploadURL = await b2.getUploadUrl(process.env.B2_BUCKET_ID);
 
-    req.files = files;
-
-    // Add the rest of your handler code here (Backblaze B2 upload logic)
-    const apiUrl = "https://api.backblazeb2.com/b2api/v2/";
-    const authorizationToken = process.env.ACCOUNT_AUTH_TOKEN;
-    const accountId = process.env.ACCOUNT_ID;
-    const bucketId = process.env.BUCKET_ID;
-    const bucketName = process.env.BUCKET_NAME;
-
-    const glbFile = req.files?.glbFile;
-
-    if (!glbFile) {
-      return res.status(400).json({ message: "File not found" });
-    }
-
-    // Get the upload URL and authorization token
-    const uploadUrlRes = await fetch(`${apiUrl}b2_get_upload_url`, {
-      method: "POST",
-      headers: {
-        "Authorization": authorizationToken,
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        accountId: accountId,
-        bucketId: bucketId,
-      }),
-    });
-    const uploadUrlData = await uploadUrlRes.json();
-
-    // Prepare the .glb file for uploading
-    const fileFormData = new FormData();
-    fileFormData.append("file", glbFile.data, {
-      filename: glbFile.name,
-      contentType: glbFile.mimetype,
+    // Upload the file
+    const uploadFileResponse = await b2.uploadFile({
+      uploadUrl: uploadURL.data.uploadUrl,
+      uploadAuthToken: uploadURL.data.authorizationToken,
+      filename: fileName,
+      data: Buffer.from(fileContent, 'binary')
     });
 
-    // Upload the .glb file to the Backblaze B2 bucket
-    const fileRes = await fetch(uploadUrlData.uploadUrl, {
-      method: "POST",
-      headers: {
-        "Authorization": uploadUrlData.authorizationToken,
-        "X-Bz-File-Name": encodeURIComponent(glbFile.name),
-        "Content-Type": glbFile.mimetype,
-        "X-Bz-Content-Sha1": "do_not_verify",
-      },
-      body: fileFormData,
-    });
-
-    if (fileRes.status === 200) {
-      // Generate the public URL for the uploaded .glb file
-      const fileUrl = `https://${bucketName}.s3.us-west-002.backblazeb2.com/${encodeURIComponent(glbFile.name)}`;
-      return res.status(200).json({ fileUrl });
-    } else {
-      return res.status(500).json({ message: "Error uploading file" });
-    }
-  } catch (err) {
-    res.statusCode = err.statusCode || 500;
-    res.end(err.message);
+    res.status(200).json({ success: true, message: 'File uploaded successfully.' });
+  } else {
+    res.status(405).json({ success: false, message: 'Method not allowed.' });
   }
-}
+};
